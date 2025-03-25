a301c5b52d2609769ddd35c290f3a183ce88cc0e
Luca Marchi <luca1010.marchi@gmail.com>
2025-03-25 17:01:38 +0100

    added HomeScreen

--- TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/HomeScreen.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/HomeScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,12 +15,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -33,24 +39,42 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(){
+    val elements = mutableListOf<String>()
+    for (i in 1..20) {
+        elements.add("Item $i")
+    }
     Scaffold(
         topBar = {
             TravelTobBar()
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
+            FloatingActionButton(onClick = {}, contentColor = MaterialTheme.colorScheme.tertiary) {
                 Icon(Icons.Filled.Add, "add item")
             }
         }
     ) {paddingValues ->
-        Column (modifier = Modifier.padding(paddingValues)){
-            TravelList()
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(paddingValues),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = (PaddingValues(8.dp, 8.dp, 8.dp, 80.dp))
+
+        ) {
+            items(elements){
+                item -> TravelListItem(item)
+            }
         }
+
     }
 }
 
@@ -59,7 +83,6 @@ fun HomeScreen(){
 fun TravelTobBar(){
     CenterAlignedTopAppBar(
         title = { Text("Travel Diary")},
-        modifier = Modifier.padding(vertical = 4.dp),
         actions = {
             IconButton(onClick = {}) {
                 Icon(Icons.Filled.Search, "Search item")
@@ -75,42 +98,34 @@ fun TravelTobBar(){
 
 }
 
-@Composable
-fun TravelList() {
-    val elements = mutableListOf<String>()
-    for (i in 1..20) {
-        elements.add("Item $i")
-    }
-
-    LazyVerticalGrid(
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        columns = GridCells.Fixed(2),
-    ) {
-        items(elements){
-            item -> TravelListItem(item)
-        }
-    }
-}
 
 @Composable
 fun TravelListItem(item: String){
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.surfaceVariant)
-            .fillMaxWidth()
-            .padding(vertical = 4.dp, horizontal = 16.dp)
-            .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(5.dp)),
-        contentAlignment = Alignment.Center
-    ){
-        Column {
-            Icon(
-                Icons.Filled.Image,
-                "element image",
-                modifier = Modifier.size(60.dp)
-            )
-            Text(item)
+    Card(
+        modifier = Modifier.size(150.dp).fillMaxWidth(),
+        colors = CardDefaults.cardColors(contentColor = MaterialTheme.colorScheme.surfaceVariant)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
+        ) {
+            Image(
+                Icons.Outlined.Image,
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .padding(10.dp),
+                contentDescription = "Travel Image"
+
+            )
+            Spacer(Modifier.size(10.dp))
+            Text(
+                item,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
         }
     }
 }
