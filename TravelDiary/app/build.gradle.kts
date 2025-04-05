e45038d123d401a685478f5f541ee796a609800f
Luca Marchi <luca1010.marchi@gmail.com>
2025-04-05 13:03:17 +0200

    chore: add viewmodel libraries

--- TravelDiary/app/build.gradle.kts
+++ TravelDiary/app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
