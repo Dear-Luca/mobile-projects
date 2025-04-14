fa8ac18498b4029b399971c0ead150ae8aac9682
Luca Marchi <luca1010.marchi@gmail.com>
2025-04-14 11:17:47 +0200

    feat: location

--- GPS/app/src/main/java/com/example/gps/MainActivity.kt
+++ GPS/app/src/main/java/com/example/gps/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gps.ui.theme.GPSTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GPSTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GPSScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun GPSScreen(){
+    val snackbarHostState = remember { SnackbarHostState() }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GPSTheme {
-        Greeting("Android")
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState)},
+        modifier = Modifier.fillMaxSize()
+    ) {
+        innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(innerPadding).fillMaxSize()
+        ) {
+            Button(onClick = {}) {
+                Text("Get current location")
+            }
+            Spacer(Modifier.height(16.dp))
+            Text("Latitude: -")
+            Text("Longitude: -")
+        }
     }
 }
